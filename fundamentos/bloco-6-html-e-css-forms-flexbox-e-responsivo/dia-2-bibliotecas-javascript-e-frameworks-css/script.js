fcebcbbf70a6d9e8ba56d9a73536421eaa91d747
Felipe <felipe__andrew@hotmail.com>
2022-07-14 22:28:29 -0300

    Exercício dia 6.2

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,15 @@
+let moment = require('moment')
+
+
+let idade = moment('26021991', 'DDMMYYYY')
+let fromNow = idade.fromNow();
+console.log('A idade é:' + fromNow);
+
+// 1 -  Adicione uma biblioteca JavaScript de date picker ao formulário que você construiu na última aula. Utilize essa biblioteca no campo "Data de início" do formulário. Você pode remover as validações de data que adicionou, uma vez que a biblioteca se encarregará de permitir somente datas válidas.
+
+
+
+
+// 2 - Adicione uma biblioteca JavaScript de validações ao formulário que você construiu na última aula. Utilize essa biblioteca para substituir as validações que você fez manualmente.
+
+// Sugestões: Just-validate e popup-validation
